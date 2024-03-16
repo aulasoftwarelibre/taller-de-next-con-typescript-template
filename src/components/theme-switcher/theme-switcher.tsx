31f1c2e89f7deca370f7577144500bbf34f950f8
Sergio Gómez Bachiller <sergio@uco.es>
2024-03-17 00:11:18 +0100

    feat(components): add theme switcher component

---
+++ src/components/theme-switcher/theme-switcher.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
+import { Switch } from '@nextui-org/react'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+
+/**
+ * Theme dark/light switcher
+ */
+export function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false)
+  const { setTheme, theme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [setMounted])
+
+  if (!mounted) return null
+
+  return (
+    <Switch
+      defaultSelected={theme === 'light'}
+      size="md"
+      color="secondary"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      startContent={<SunIcon />}
+      endContent={<MoonIcon />}
+    />
+  )
+}
